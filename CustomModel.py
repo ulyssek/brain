87ef3cc03fb61cade3be1634899ddafdfb56de88
hulyce <ulysseklatzmann@gmail.com>
2015-07-09 17:09:30 +0200

    refactoring

--- CustomModel.py
+++ CustomModel.py
@@ -35,7 +35,7 @@ class BrandAverage(Model):
 
       count = 0
       brands = {}
-      limit = None
+      limit = 10
 
       if limit is not None:
         train_len = min(TRAIN_LEN,limit)
@@ -87,79 +87,9 @@ class BrandAverage(Model):
       cat = self.cat_from_brand(brand)
       return cat
 
-
-
     def cat_from_brand(self,brand):
       brand_dict = self.brands[brand]
       return max(brand_dict.keys(),key=lambda x : brand_dict[x])
-
-    
-    def compute_output(self):
-      result = [["Id_Produit","Id_Categorie"]]
-      id_position = ID_POSITION
-      if self.train:
-        file_name       = VALIDATION_FILE
-        brand_position  = BRAND_POSITION
-        file_len        = TRAIN_LEN
-        validation_len  = VALIDATION_LEN
-        cat_position    = C3_ID_POSITION
-      else:
-        file_name       = TEST_FILE
-        brand_position  = BRAND_POSITION_TEST
-        file_len        = TEST_LEN
-
-      spam_reader = parser(file_name)
-      count = 0
-      score = 0
-      limit = None
-
-      if limit is not None:
-        file_len = min(file_len,limit)
-      else:
-        file_len = file_len
- 
-
-      print "computing output"
-      next(spam_reader)
-      for item in spam_reader:
-        cat = self.compute_category(item)
-        if self.train:
-          real_cat = item[cat_position]
-          score += int(real_cat == cat)
-        else:  
-          result.append([item[ID_POSITION]])
-        count += 1
-        if not(int(count) % int(file_len/10)):
-          print "%s%% done" % (100*count/float(file_len),)
-        if (limit is not None) & (count == limit):
-          break
-      print "100% done"
-      if self.train:
-        self.score = score/float(validation_len)*100
-        print "score : %s " % (self.score,)
-      else:
-        self.result = result
-        
-    ##################################################################################
-    ## PRINTING FUNCTIONS
-
-    def print_output(self):
-      print "Writing results in output file"
-
-      output_file = open(self.output_name, 'w')
-      a = csv.writer(output_file,delimiter=DELIMITER)
-      a.writerows(self.result)
-      output_file.close()
-
-    ##################################################################################
-    ## RUNNNING FUNCTIONS
-
-    def run(self):
-      self.build()
-      self.compute_output()
-      if not self.train:
-        self.print_output()
-
 
 
 
