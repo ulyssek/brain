1f75a58b69b724ae9ae3aa9e541c13604284b8c3
annelor <annelaurepoite@gmail.com>
2015-07-09 17:11:29 +0200

    modele prix

--- CustomModel.py
+++ CustomModel.py
@@ -163,5 +163,135 @@ class BrandAverage(Model):
 
 
 
+class PriceAverage(Model):
+
+    ##################################################################################
+    ## INIT FUNCTIONS
+
+    def __init__(self,train=False,**kwargs):
+      self.train = train
+      self.score = 0
+      Model.__init__(self,**kwargs)
+      self.name = "PRICE_AVERAGE"
+      self.output_name = RESULT_PATH + self.name + ".csv"
+
+    ##################################################################################
+    ## BUILDING FUNCTIONS
+
+    def build(self):
+      path = TRAIN_FILE
+      train_len = TRAIN_LEN
+      price_position = PRICE_POSITION
+      id_position = C3_ID_POSITION
+
+      count = 0
+      limit = None
+
+      if limit is not None:
+        train_len = min(TRAIN_LEN,limit)
+      else:
+        train_len = TRAIN_LEN
+ 
+      spam_reader = parser(path)
+
+      print "computing brands dictionary"
+
+      for row in spam_reader:
+        brand = row[brand_position]
+        if brand == '':
+          brand = no_brand
+       
+        if brand in brands.keys():
+          if row[id_position] in brands[brand].keys():
+            brands[brand][row[id_position]] += 1
+          else:
+            brands[brand][row[id_position]] = 1
+        else:
+          brands[brand] = {row[id_position] : 1}
+        count += 1
+        if limit is not None and count == limit:
+          break
+        if not(int(count) % int(train_len/10)):
+          print "%s%% done" % (100*count/float(train_len),)
+      print "100% done"
+
+      self.brands = brands
+
+
+
+    ##################################################################################
+    ## CATEGORY COMPUTING FUNCTIONS
+
+    def compute_category(self,item):
+      #Core function, associating an item with a category
+      #item is a vector just read from the file
+      if self.train:
+        brand_position = BRAND_POSITION
+      else:
+        brand_position = BRAND_POSITION_TEST
+      no_brand = NO_BRAND
+      if item[brand_position] not in self.brands.keys():
+        brand = no_brand
+      else:
+        brand = item[brand_position]
+      cat = self.cat_from_brand(brand)
+      return cat
+
+
+
+    def cat_from_brand(self,brand):
+      brand_dict = self.brands[brand]
+      return max(brand_dict.keys(),key=lambda x : brand_dict[x])
+
+    
+    def compute_output(self):
+      result = [["Id_Produit","Id_Categorie"]]
+      id_position = ID_POSITION
+      if self.train:
+        file_name       = VALIDATION_FILE
+        brand_position  = BRAND_POSITION
+        file_len        = TRAIN_LEN
+        validation_len  = VALIDATION_LEN
+        cat_position    = C3_ID_POSITION
+      else:
+        file_name       = TEST_FILE
+        brand_position  = BRAND_POSITION_TEST
+        file_len        = TEST_LEN
+
+      spam_reader = parser(file_name)
+      count = 0
+      score = 0
+      limit = None
+
+      if limit is not None:
+        file_len = min(file_len,limit)
+      else:
+        file_len = file_len
+ 
+
+      print "computing output"
+      next(spam_reader)
+      for item in spam_reader:
+        cat = self.compute_category(item)
+        if self.train:
+          real_cat = item[cat_position]
+          score += int(real_cat == cat)
+        else:  
+          result.append([item[ID_POSITION]])
+        count += 1
+        if not(int(count) % int(file_len/10)):
+          print "%s%% done" % (100*count/float(file_len),)
+        if (limit is not None) & (count == limit):
+          break
+      print "100% done"
+      if self.train:
+        self.score = score/float(validation_len)*100
+        print "score : %s " % (self.score,)
+      else:
+        self.result = result
+
+
+
+
 
  
